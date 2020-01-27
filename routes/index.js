2cb0678e67f3b7d5a7b6b4e3c97a22649502b33b
david <david@MacBook-Pro-de-david.local>
2020-01-27 02:07:08 +0100

    initial project

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 /* GET tasks page. */
 router.get('/tasks-page', function(req, res, next) {
-  res.render('tasks', { title: 'Express' });
+  res.render('tasks');
 });
 
 /* GET Messages page. */
 router.get('/messages-page', function(req, res, next) {
-  res.render('messages', { title: 'Express' });
+  res.render('messages');
 });
 
-/* GET Messages page. */
+/* GET Users page. */
 router.get('/users-page', function(req, res, next) {
-  res.render('users', { title: 'Express' });
+  res.render('users');
 });
 
-/* GET Messages page. */
+/* GET Catalog page. */
 router.get('/catalog-page', function(req, res, next) {
-  res.render('catalog', { title: 'Express' });
+  res.render('catalog');
 });
 
-/* GET Messages page. */
+/* GET Orders-list page. */
 router.get('/orders-list-page', function(req, res, next) {
-  res.render('orders-list', { title: 'Express' });
+  res.render('orders-list');
 });
 
-/* GET Messages page. */
+/* GET Order detail page. */
 router.get('/order-page', function(req, res, next) {
-  res.render('order', { title: 'Express' });
+  res.render('order');
+});
+
+/* GET chart page. */
+router.get('/charts', function(req, res, next) {
+  res.render('charts');
 });
 
 
